1b0ba596145345f5cfb555ec6bcb24b1e57ed0de
marbypangilinan <marbysneaker@gmail.com>
2023-03-08 03:22:02 -0800

    finished app

--- app/src/main/java/com/example/marbysandroidproject/MainActivity.java
+++ app/src/main/java/com/example/marbysandroidproject/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.marbysandroidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -11,17 +12,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Project gettingStartedProject = new Project("my project","my project description",1234);
+        RecyclerView list = findViewById(R.id.recycle_view_projects);
+
+
 
         Project[] projects = {
 
-                new Project("Getting Started app","my project description",1234),
-                new Project("Bmi Calculator","my project description",1234),
-                new Project("Inches converter","my project description",1234),
-                new Project("Name getter app","my project description",1234),
-                new Project("Developer Restaurant","my project description",1234)
+                new Project("Getting Started app","my project description",R.drawable.getting_started),
+                new Project("Bmi Calculator","my project description",R.drawable.calculator),
+                new Project("Inches converter","my project description",R.drawable.tape),
+                new Project("Name getter app","my project description",R.drawable.quote),
+                new Project("Developer Restaurant","my project description",R.drawable.hungry_developer)
         };
 
+        ProjectsAdapter projectAdapter = new ProjectsAdapter(projects);
+
+        list.setAdapter(projectAdapter);
     }
 
 }
